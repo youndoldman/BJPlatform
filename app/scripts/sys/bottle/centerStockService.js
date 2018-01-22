8646d2f19e13f6a10fd94e23e2b4678bad7b2db2
zhouy <jack_zhouy@163.com>
2018-01-22 10:39:32 +0800

    门店及充气站的库存管理！

--- app/scripts/sys/bottle/centerStockService.js
+++ app/scripts/sys/bottle/centerStockService.js
@@ -1,6 +1,6 @@
 'use strict';
 
-shopManageApp.service('ShopStockService', ['$http', 'URI', 'promiseWrapper','MISC', function ($http, URI, promise,MISC) {
+bottleApp.service('CenterStockService', ['$http', 'URI', 'promiseWrapper','MISC', function ($http, URI, promise,MISC) {
     var bottlesUri = URI.resources.bottle;//钢瓶接口
     var bottleHandOverUri = URI.resources.bottleHandOver;//钢瓶交接接口
 
