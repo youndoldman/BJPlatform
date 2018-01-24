11eabe4bc2349323493d60b3fa97dfd2110402c0
wyb <104855264@qq.com>
2018-01-24 09:17:20 +0800

    线上环境netty冲突，屏蔽与netty、udp接收相关的代码

--- src/main/java/com/donno/nj/net/UdpServerHandler.java
+++ src/main/java/com/donno/nj/net/UdpServerHandler.java
@@ -1,35 +1,35 @@
 package com.donno.nj.net;
 
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.channel.socket.DatagramPacket;
-import io.netty.util.CharsetUtil;
+//import io.netty.buffer.Unpooled;
+//import io.netty.channel.ChannelHandlerContext;
+//import io.netty.channel.SimpleChannelInboundHandler;
+//import io.netty.channel.socket.DatagramPacket;
+//import io.netty.util.CharsetUtil;
 import org.json.JSONObject;
 
 import java.util.concurrent.ThreadLocalRandom;
 
 public class UdpServerHandler
-        extends SimpleChannelInboundHandler<DatagramPacket>
+        //extends SimpleChannelInboundHandler<DatagramPacket>
 {
 
-    @Override
-    public void messageReceived(ChannelHandlerContext channelHandlerContext,
-                                DatagramPacket datagramPacket) throws Exception
-    {
-        String revMessage = datagramPacket.content().toString(CharsetUtil.UTF_8);
-
-        JSONObject json = new JSONObject(revMessage);
-
-        /*接收数据解析*/
-        Integer openid = json.getInt("id");
-        System.out.println(openid);
-
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause)throws Exception{
-        ctx.close();
-        cause.printStackTrace();
-    }
+//    @Override
+//    public void messageReceived(ChannelHandlerContext channelHandlerContext,
+//                                DatagramPacket datagramPacket) throws Exception
+//    {
+//        String revMessage = datagramPacket.content().toString(CharsetUtil.UTF_8);
+//
+//        JSONObject json = new JSONObject(revMessage);
+//
+//        /*接收数据解析*/
+//        Integer openid = json.getInt("id");
+//        System.out.println(openid);
+//
+//    }
+//
+//    @Override
+//    public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause)throws Exception{
+//        ctx.close();
+//        cause.printStackTrace();
+//    }
 }
