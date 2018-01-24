4fe65a8c6d6ad9fcec4726c6968b895168850f57
zhouy <jack_zhouy@163.com>
2018-01-24 11:01:51 +0800

    Log4j日志问题的屏蔽！ table_store库引用了Log4j，故注释掉了！

--- src/main/java/com/donno/nj/service/UdpServerService.java
+++ src/main/java/com/donno/nj/service/UdpServerService.java
@@ -2,11 +2,11 @@ package com.donno.nj.service;
 
 import com.donno.nj.net.UdpServerHandler;
 import com.donno.nj.util.ConfigUtil;
-//import io.netty.bootstrap.Bootstrap;
-//import io.netty.channel.ChannelOption;
-//import io.netty.channel.EventLoopGroup;
-//import io.netty.channel.nio.NioEventLoopGroup;
-//import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 public interface UdpServerService extends Runnable
 {
 
-    //void run(int port) throws Exception;
+    void run(int port) throws Exception;
 
 }
--- src/main/java/com/donno/nj/service/impl/UdpServerServiceImpl.java
+++ src/main/java/com/donno/nj/service/impl/UdpServerServiceImpl.java
@@ -3,11 +3,11 @@ package com.donno.nj.service.impl;
 import com.donno.nj.net.UdpServerHandler;
 import com.donno.nj.service.UdpServerService;
 import com.donno.nj.util.ConfigUtil;
-//import io.netty.bootstrap.Bootstrap;
-//import io.netty.channel.ChannelOption;
-//import io.netty.channel.EventLoopGroup;
-//import io.netty.channel.nio.NioEventLoopGroup;
-//import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,38 +20,38 @@ public class UdpServerServiceImpl implements UdpServerService
     @Override
     public void run()
     {
-//        try
-//        {
-//            while (configUtil == null)
-//            {
-//                Thread.sleep(10000);
-//            }
-//
-//            run(configUtil.getRecvPositionPort());
-//        } catch (InterruptedException e)
-//        {
-//            e.printStackTrace();
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
+        try
+        {
+            while (configUtil == null)
+            {
+                Thread.sleep(10000);
+            }
+
+            run(configUtil.getRecvPositionPort());
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
 
     }
 
-//    public void run(int port) throws Exception
-//    {
-//        EventLoopGroup group = new NioEventLoopGroup();
-//        try {
-//            Bootstrap b = new Bootstrap();
-//            b.group(group).channel(NioDatagramChannel.class)
-//                    .option(ChannelOption.SO_BROADCAST,true)
-//                    .handler(new UdpServerHandler());
-//
-//            b.bind(port).sync().channel().closeFuture().await();
-//        }
-//        finally {
-//            group.shutdownGracefully();
-//        }
-//    }
+    public void run(int port) throws Exception
+    {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioDatagramChannel.class)
+                    .option(ChannelOption.SO_BROADCAST,true)
+                    .handler(new UdpServerHandler());
+
+            b.bind(port).sync().channel().closeFuture().await();
+        }
+        finally {
+            group.shutdownGracefully();
+        }
+    }
 }
