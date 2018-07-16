8d3d89a58277341925419ae94b855286f16b7ed9
jjz <794737319@qq.com>
2018-07-16 16:02:59 +0800

    替换config

--- app/scripts/common/config.js
+++ app/scripts/common/config.js
@@ -11,87 +11,83 @@ commonModule.constant('URI', {
     'resources': {
         'loginPage': '../pages/login.htm',//登录页
         'mainPage': '../pages/mainCenter.htm',//首页
-        'users': '../../../api/sysusers',//系统用户接口
-        'department': '../../../api/Department',//部门接口
-        'groups': '../../../api/groups',//组接口
-        'customers': '../../../api/customers',//客户接口
-        'customerSource': '../../../api/CustomerSource',//客户来源查询接口
+        'users': 'http://www.yunnanbaijiang.com/api/sysusers',//系统用户接口
+        'department': 'http://www.yunnanbaijiang.com/api/Department',//部门接口
+        'groups': 'http://www.yunnanbaijiang.com/api/groups',//组接口
+        'customers': 'http://www.yunnanbaijiang.com/api/customers',//客户接口
+        'customerSource': 'http://www.yunnanbaijiang.com/api/CustomerSource',//客户来源查询接口
         //实际使用地址
         //'customerLevel': '../../../api/CustomerLevel',//客户等级查询接口
         //'customerType': '../../../api/CustomerType',//客户类型查询接口
-        'customerLevel': '../../../api/CustomerLevel',//客户等级查询接口
-        'customerType': '../../../api/CustomerType',//客户类型查询接口
+        'customerLevel': 'http://www.yunnanbaijiang.com/api/CustomerLevel',//客户等级查询接口
+        'customerType': 'http://www.yunnanbaijiang.com/api/CustomerType',//客户类型查询接口
 
-        'settlementType': '../../../api/SettlementType',//结算类型信息查询接口
+        'settlementType': 'http://www.yunnanbaijiang.com/api/SettlementType',//结算类型信息查询接口
 
-        'discountStrategies':'../../../api/DiscountStrategies',//优惠策略接口
+        'discountStrategies':'http://www.yunnanbaijiang.com/api/DiscountStrategies',//优惠策略接口
 
-        'customerCallin':'../../../api/CustomerCallIn',//电话关联的客户资料查询接口
-        'login':'../../../api/sysusers/login',//系统用户登录接口
-        'logout':'../../../api/sysusers/logout',//系统用户登出接口
-        'orders':'../../../api/Orders',　　　　　//订气订单接口
+        'customerCallin':'http://www.yunnanbaijiang.com/api/CustomerCallIn',//电话关联的客户资料查询接口
+        'login':'http://www.yunnanbaijiang.com/api/sysusers/login',//系统用户登录接口
+        'logout':'http://www.yunnanbaijiang.com/api/sysusers/logout',//系统用户登出接口
+        'orders':'http://www.yunnanbaijiang.com/api/Orders',　　　　　//订气订单接口
         'orderCancel':'../../../api/CancelOrder',　　　　　//订气作废接口
 
         'taskOrders':'../../../api/TaskOrders',//任务订单接口
         'taskOrdersDeal':'../../../api/TaskOrders/Process',//任务订单办理接口
         //'goods': '../../../api/Goods',//商品
         //'goodsTypes': '../../../api/GoodsTypes',//商品类型
-        'goods': '../../../api/Goods',//商品
-        'goodsTypes': '../../../api/GoodsTypes',//商品类型
+        'goods': 'http://www.yunnanbaijiang.com/api/Goods',//商品
+        'goodsTypes': 'http://www.yunnanbaijiang.com/api/GoodsTypes',//商品类型
 
-        'goodsPriceHistory': '../../../api/Goods/PriceHistory',//商品价格历史
-        'subdistrict':'https://restapi.amap.com/v3/config/district',//高德地图行政区域接口
-        'geocode':'https://restapi.amap.com/v3/geocode/geo',//高德地图逆地址解码接口
-        'bottle': '../../../api/GasCylinder',//钢瓶接口
+        'goodsPriceHistory': 'http://www.yunnanbaijiang.com/api/Goods/PriceHistory',//商品价格历史
+        'subdistrict':'http://restapi.amap.com/v3/config/district',//高德地图行政区域接口
+        'geocode':'http://restapi.amap.com/v3/geocode/geo',//高德地图逆地址解码接口
+        'bottle': 'http://www.yunnanbaijiang.com/api/GasCylinder',//钢瓶接口
         'bottleHandOver': '../../../api/GasCylinder/TakeOver',//钢瓶责任交接
         'gpsBind': '../../../api/GasCylinder/Bind',//定位终端绑定接口
         'gpsUnBind': '../../../api/GasCylinder/UnBind',//定位终端解除绑定接口
-        'bottleSpecQuery': '../../../api/GasCylinderSpec',//钢瓶规格查询
-        'cloudUser': '../../../api/CloudUser',//云客服用户接口
-        'cloudUserBind': '../../../api/CloudUser/Bind',//云客服用户绑定接口
-        'cloudUserUnBind': '../../../api/CloudUser/UnBind',//云客服用户解除绑定接口
+        'bottleSpecQuery': 'http://www.yunnanbaijiang.com/api/GasCylinderSpec',//钢瓶规格查询
+        'cloudUser': 'http://www.yunnanbaijiang.com/api/CloudUser',//云客服用户接口
+        'cloudUserBind': 'http://www.yunnanbaijiang.com/api/CloudUser/Bind',//云客服用户绑定接口
+        'cloudUserUnBind': 'http://www.yunnanbaijiang.com/api/CloudUser/UnBind',//云客服用户解除绑定接口
         //'adjustPriceSchedules': '../../../api/AdjustPriceSchedules',//调价接口
-        'adjustPriceSchedules': '../../../api/AdjustPriceSchedules',//调价接口
+        'adjustPriceSchedules': 'http://www.yunnanbaijiang.com/api/AdjustPriceSchedules',//调价接口
 
-        'ticket': '../../../api/Ticket',//气票信息增加
-        'coupon': '../../../api/Coupon',//优惠券信息增加
+        'ticket': 'http://www.yunnanbaijiang.com/api/Ticket',//气票信息增加
+        'coupon': 'http://www.yunnanbaijiang.com/api/Coupon',//优惠券信息增加
 
-        'salesByBayType': '../../../api/Report/Sales/ByPayType',//查询销售日报表(按支付类型查询)
-        'salesByCustomerType': '../../../api/Report/Sales/ByCustomerType',//查询销售日报表(按支付类型查询)
-        'saleContacts':'../../../api/Report/SaleContacts',//销售往来日报表查询
-        'stock':'../../../api/Report/Stock',//查询库存
-        'stockInOut':'../../../api/Report/StockInOut',//查询出入库数量
-        'salesCash':'../../../api/Report/SaleCash',//销售现金报表
+        'salesByBayType': 'http://www.yunnanbaijiang.com/api/Report/Sales/ByPayType',//查询销售日报表(按支付类型查询)
+        'salesByCustomerType': 'http://www.yunnanbaijiang.com/api/Report/Sales/ByCustomerType',//查询销售日报表(按支付类型查询)
+        'saleContacts':'http://www.yunnanbaijiang.com/api/Report/SaleContacts',//销售往来日报表查询
+        'stock':'http://www.yunnanbaijiang.com/api/Report/Stock',//查询库存
+        'stockInOut':'http://www.yunnanbaijiang.com/api/Report/StockInOut',//查询出入库数量
+        'salesCash':'http://www.yunnanbaijiang.com/api/Report/SaleCash',//销售现金报表
 
+        'mendType': 'http://www.yunnanbaijiang.com/api/MendTypes',//报修类型接口
+        'mend': 'http://www.yunnanbaijiang.com/api/Mend',//报修单接口
+        'securityType': 'http://www.yunnanbaijiang.com/api/SecurityTypes',//安检类型接口
+        'security': 'http://www.yunnanbaijiang.com/api/Security',//安检单接口
+        'complaintType': 'http://www.yunnanbaijiang.com/api/ComplaintTypes',//投诉类型接口
+        'complaint': 'http://www.yunnanbaijiang.com/api/Complaint',//投诉单接口
 
-        'mendType': '../../../api/MendTypes',//报修类型接口
-        'mend': '../../../api/Mend',//报修单接口
-        'securityType': '../../../api/SecurityTypes',//安检类型接口
-        'security': '../../../api/Security',//安检单接口
-        'complaintType': '../../../api/ComplaintTypes',//投诉类型接口
-        'complaint': '../../../api/Complaint',//投诉单接口
-
-        'customerCredit': '../../../api/CustomerCredit',//欠款
-        'writeOff':'../../../api/WriteOff',//回款
-        'writeOffDetail':'../../../api/WriteOffDetail',//回款明细信息查询
-        'depositDetail':'../../../api/DepositDetail',//增加存银行款信息记录
-
-        'gasCylinderSpec':'../../../api/GasCylinderSpec',//查询钢瓶规格
-        'addGasCylinderSpec':'../../../api/GasCyrDynDetail',//钢检瓶动态增加
-        'reportGasCyrDyn':'../../../api/Report/GasCyrDyn',//钢检瓶报表查询
-        'gasCyrChargeSpec':'../../../api/GasCyrChargeSpec',//钢瓶收费标准查询
+        'customerCredit': 'http://www.yunnanbaijiang.com/api/CustomerCredit',//欠款
+        'writeOff':'http://www.yunnanbaijiang.com/api/WriteOff',//回款
+        'writeOffDetail':'http://www.yunnanbaijiang.com/api/WriteOffDetail',//回款明细信息查询
+        'depositDetail':'http://www.yunnanbaijiang.com/api/DepositDetail',//增加存银行款信息记录
 
         'gasCylinderPosition':'../../../api/GasCylinderPosition',//钢瓶轨迹查询
         'gasCylinderTakeOverHistory':'../../../api/GasCylinder/TakeOver/History',//钢瓶交接记录查询
 
         'GasCylinderWarn':'../../../api/GasCylinderWarn',//告警查询
+        'gasCylinderSpec':'http://www.yunnanbaijiang.com/api/GasCylinderSpec',//查询钢瓶规格
+        'addGasCylinderSpec':'http://www.yunnanbaijiang.com/api/GasCyrDynDetail',//钢检瓶动态增加
+        'reportGasCyrDyn':'http://www.yunnanbaijiang.com/api/Report/GasCyrDyn',//钢检瓶报表查询
+        'gasCyrChargeSpec':'http://www.yunnanbaijiang.com/api/GasCyrChargeSpec',//钢瓶收费标准查询
 
-        'GasCynTrayBind':'../../../api/GasCynTray/Bind',//托盘绑定
-        'GasCynTray':'../../../api/GasCynTray',//托盘查询
-        'GasCynTrayunBind':'../../../api/GasCynTray/unBind',//托盘解绑定
-        'GasCynTrayHistory':'../../../api/GasCynTray/History',//托盘历史数据查询
-
-
+        'GasCynTrayBind':'http://www.yunnanbaijiang.com/api/GasCynTray/Bind',//托盘绑定
+        'GasCynTray':'http://www.yunnanbaijiang.com/api/GasCynTray',//托盘查询
+        'GasCynTrayunBind':'http://www.yunnanbaijiang.com/api/GasCynTray/unBind',//托盘解绑定
+        'GasCynTrayHistory':'http://www.yunnanbaijiang.com/api/GasCynTray/History',//托盘历史数据查询
 
         'ktyAuthenticate': 'https://cc.ketianyun.com/thirdparty/api/v1/authenticate',//科天云登录接口
         'ktyReport1': 'https://cc.ketianyun.com/thirdparty/api/v1/callsdata/agent/call',//科天云获取坐席-工作组及直拨电话统计报表
@@ -103,7 +99,6 @@ commonModule.constant('URI', {
 
         'ktyCallreport': 'https://cc.ketianyun.com/thirdparty/api/v1/callreports',//科天云获得指定时间段呼叫的记录
         'ktyCallreportSearch': 'https://cc.ketianyun.com/thirdparty/api/v1/callreports/search',//科天云获得获取指定搜索条件的呼叫记录
-
 
 
     }
@@ -241,9 +236,9 @@ commonModule.constant('NavItem', {
             target: "_blank",
             href: "./shopCenter.htm#/ShopManage/calculateDelivery",
             icon: "fa-table"
-        }]
+        }
+        ]
     },
-
     'CustomService': {
         roles:[1,2],
         name: "客服业务",
@@ -414,25 +409,18 @@ commonModule.constant('NavItem', {
             href: "./decisionCenter.htm#/decision/cost",
             icon: "fa-group"
         }, {
-                index: 1,
-                name: "用气统计",
-                href: "./decisionCenter.htm#/decision/gasUsage",
-                icon: "fa-gears"
-        }
-            //{
-        //    index: 1,
-        //    name: "销售分析",
-        //    target: "_blank",
-        //    href: "./decisionCenter.htm#/decision/sales",
-        //    icon: "fa-plane"
-        //}, {
-        //    index: 2,
-        //    name: "市场分析",
-        //    target: "_blank",
-        //    href: "./decisionCenter.htm#/decision/market",
-        //    icon: "fa-magnet"
-        //}
-        ]
+            index: 1,
+            name: "销售分析",
+            target: "_blank",
+            href: "./decisionCenter.htm#/decision/sales",
+            icon: "fa-plane"
+        }, {
+            index: 2,
+            name: "市场分析",
+            target: "_blank",
+            href: "./decisionCenter.htm#/decision/market",
+            icon: "fa-magnet"
+        }]
     },
 
     'GasCenter': {
@@ -449,13 +437,8 @@ commonModule.constant('NavItem', {
             name: "钢瓶档案",
             href: "./gasCenter.htm#/bottles/list",
             icon: "fa-building"
-        }, {
-            index: 2,
-            name: "流转异常",
-            href: "./gasCenter.htm#/bottles/warning",
-            icon: "fa-exclamation-triangle"
         },{
-            index: 3,
+            index: 1,
             name: "库存管理",
             href: "./gasCenter.htm#/bottles/stock",
             icon: "fa-magnet"
