7a0277e797f0db71d2fbdf4c157df35b1de448d2
wyb <104855264@qq.com>
2018-08-19 10:07:48 +0800

    优惠策略修改，修改按3级客户优惠未生效的bug

--- src/main/java/com/donno/nj/service/impl/OrderServiceImpl.java
+++ src/main/java/com/donno/nj/service/impl/OrderServiceImpl.java
@@ -329,6 +329,7 @@ public class OrderServiceImpl implements OrderService
         params.putAll(ImmutableMap.of("endTime", new Date()));
 
         List<DiscountStrategy>  discountStrategies = discountStrategyDao.getList(params);
+        Boolean findTarget = false;
         for (DiscountStrategy discountStrategy : discountStrategies)
         {
             if ( (discountStrategy.getDiscountConditionType().getCode().equals(ServerConstantValue.DISCOUNT_CONDITION_TYPE_CUSTOMER_LEVEL)
@@ -345,11 +346,13 @@ public class OrderServiceImpl implements OrderService
                         if (discountStrategy.getDiscountType() == DiscountType.DTCheapX )//直减
                         {
                             dealPrice = dealPrice - discountDetail.getDiscount();
+                            findTarget = true;
                             break;
                         }
                         else if(discountStrategy.getDiscountType() == DiscountType.DTCheapX )//百分比折扣
                         {
                             dealPrice = dealPrice - dealPrice * discountDetail.getDiscount() / 100;
+                            findTarget = true;
                             break;
                         }
                     }
@@ -357,9 +360,12 @@ public class OrderServiceImpl implements OrderService
             }
 
             /*优惠是否叠加*/
-            if (discountStrategy.getUseType() == DiscountUseType.DUTExclusive)
+            if (findTarget)
             {
-                break;
+                if (discountStrategy.getUseType() == DiscountUseType.DUTExclusive)
+                {
+                    break;
+                }
             }
         }
 
