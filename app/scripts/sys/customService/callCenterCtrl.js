2e9edf63d5093541734ced8db34267ea56df104c
zhouy <jack_zhouy@163.com>
2018-09-18 12:05:21 +0800

    订单创建成功后清除来电号码！

--- app/scripts/sys/customService/callCenterCtrl.js
+++ app/scripts/sys/customService/callCenterCtrl.js
@@ -357,6 +357,8 @@ customServiceApp.controller('CallCenterCtrl', ['$scope', '$rootScope', '$filter'
             //将订单数据提交到后台
             OrderService.createOrder($scope.currentOrder).then(function () {
                 udcModal.info({"title": "处理结果", "message": "创建订单成功 "});
+                //订单完成后清除来电号码
+                $scope.vm.callInPhone = "00000000";
             }, function(value) {
                 udcModal.info({"title": "处理结果", "message": "创建订单失败 "+value.message});
             })
